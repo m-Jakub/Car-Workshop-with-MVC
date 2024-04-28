bd7fd33a92a3fd5126708cff9f5215c4a89a8aa5
Kuba <xkubaxmx@gmail.com>
2024-04-28 15:33:28 +0200

    Add initial migration to set up database schema

---
+++ CarWorkshop/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace CarWorkshop.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(100)]
+        [MaxLength(100)]
+        [Required]
+        public string? Name { get; set; }
+    }
+}
---
+++ CarWorkshop/ViewModels/RegisterVM.cs
@@ -0,0 +1,6 @@
+﻿namespace CarWorkshop.ViewModels
+{
+    public class RegisterVM
+    {
+    }
+}
