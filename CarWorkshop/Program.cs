f395cc84aceedb41ff284abc1c03f0470219571c
Kuba <xkubaxmx@gmail.com>
2024-04-29 13:41:01 +0200

    Set up roles and admin user creation at startup

--- CarWorkshop/Program.cs
+++ CarWorkshop/Program.cs
@@ -26,6 +26,58 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(
 
 var app = builder.Build();
 
+async Task InitializeRolesAndUsers(IServiceProvider serviceProvider)
+{
+    var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var UserManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+
+    if (!await RoleManager.RoleExistsAsync("Admin"))
+        await RoleManager.CreateAsync(new IdentityRole("Admin"));
+
+    if (!await RoleManager.RoleExistsAsync("Employee"))
+        await RoleManager.CreateAsync(new IdentityRole("Employee"));
+
+    var adminUser = await UserManager.FindByEmailAsync("admin");
+    if (adminUser == null)
+    {
+        adminUser = new AppUser { Name = "Admin", UserName = "admin@example.com", Email = "admin@example.com" };
+        var result = await UserManager.CreateAsync(adminUser, "Admin123");
+        if (!result.Succeeded)
+        {
+			foreach (var error in result.Errors)
+			{
+				Console.WriteLine($"Error: {error.Code} - {error.Description}");
+			}
+			throw new Exception("Cannot create admin user");
+        }
+    }
+
+
+    if (!await UserManager.IsInRoleAsync(adminUser, "Admin"))
+        await UserManager.AddToRoleAsync(adminUser, "Admin");
+
+    var otherUsers = await UserManager.Users.ToListAsync();
+    foreach (var user in otherUsers)
+    {
+        if (user.UserName != "admin@example.com" && !await UserManager.IsInRoleAsync(user, "Employee"))
+            await UserManager.AddToRoleAsync(user, "Employee");
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        await InitializeRolesAndUsers(serviceProvider);
+    }
+    catch (Exception ex)
+    {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -36,9 +88,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
