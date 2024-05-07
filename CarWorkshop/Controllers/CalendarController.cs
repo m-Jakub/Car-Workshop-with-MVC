2cfef7fb9c8b4e00854e7b5e753b471588c5e867
Kuba <xkubaxmx@gmail.com>
2024-05-07 20:10:17 +0200

    Add hover effect to calendar table cells

--- CarWorkshop/Controllers/CalendarController.cs
+++ CarWorkshop/Controllers/CalendarController.cs
@@ -1,18 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-
-namespace CarWorkshop.Controllers
-{
-    public class CalendarController : Controller
-    {
-        [HttpPost]
-        public IActionResult AddAvailabilitySlot(DateTime date, int hour)
-        {
-            // Add logic to handle adding availability slot
-            // Example: Save the date and hour to the database
-
-            // Return a success response
-            return Ok("Availability slot added successfully");
-        }
-    }
-}
+﻿
